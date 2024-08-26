a133070a8a27f6699ad9e033c2e1ba3474ef1ee5
Ignacio Alfaro Pinedo <Ignacio.AlfaroPinedo@enghouse.com>
2024-08-26 19:55:27 +0200

    Agregar archivos de proyecto.

---
+++ Curso_Basico/Helpers/LectorDeFicheros.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace Curso_Basico.Helpers
+{
+    public class LectorFicheros
+    {
+        private const string EXCEPCION_FICHERO_NO_EXISTE = "No existe fichero y no se puede leer. Obvio!";
+
+        private bool _Existe_Fichero(string ruta)
+        {
+            return File.Exists(ruta);
+        }
+
+        /// <summary>
+        /// Leer un archivo y devuelve por consola si existe el contenido del fichero o mensaje de excepción.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void Leer(string filePath)
+        {
+
+            // Constantes
+            const string EXCEPCION_FICHERO_SIN_CONTENIDO = "Existe pero no se puede leer el contenido.";
+
+            // 0. Inicializar variables
+            string resultado = null;
+
+            // 1. Comprobar si el fichero existe
+            bool existe = _Existe_Fichero(filePath);
+
+            // 2. Leerlo
+            if (existe)
+            {
+                try
+                {
+                    resultado = File.ReadAllText(filePath);
+                }
+                catch (Exception ex) { }
+            }
+
+            // 3. Construir el resultado
+            resultado = (existe) ? (resultado == null) ? EXCEPCION_FICHERO_SIN_CONTENIDO : resultado : EXCEPCION_FICHERO_NO_EXISTE;
+
+            // 4. Mostrar el resultado
+            Console.Write("La función devuelve: " + resultado);
+
+        }
+
+        /// <summary>
+        /// Lee un archivo para conocer el MIME y devuelve en consola el MIME o mensaje de excepción.
+        /// </summary>
+        /// <param name="filePath"></param>
+        internal void LeerMIME(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Comprueba el tamaño del archivo y devuelve en consola el tamaño o mensaje de excepción.
+        /// </summary>
+        /// <param name="rutaArchivo"></param>
+        public void Tamaño(string rutaArchivo)
+        {
+            // Constantes
+            const string EXCEPCION_FICHERO_SIN_TAMAÑO = "El fichero existe pero hubo un problema para obtener su tamaño.";
+
+            // 0. Inicializar variables
+            FileInfo fileInfo = null;
+
+            // 1. Comprobar si el fichero existe
+            bool existe = _Existe_Fichero(rutaArchivo);
+
+            // 2. Obtener la información
+            if (existe)
+            {
+                try
+                {
+                    fileInfo = new FileInfo(rutaArchivo);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            // 3. Formatear la salida
+            string resultado = (existe) ? (fileInfo == null) ? EXCEPCION_FICHERO_SIN_TAMAÑO : $"El tamaño del archivo \"{rutaArchivo}\" es {fileInfo.Length} bytes." : EXCEPCION_FICHERO_NO_EXISTE;
+
+            // 4. Mostrar el resultado
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ Curso_Basico/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Curso_Basico.Helpers;
+
+
+namespace Curso_Basico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var _filePath = "C:\\Test\\nuevo5.txt"; // Cambia esto a la ruta de tu archivo    
+            var _filePath = "C:\\Test\\nuevo5.txt"; // Cambia esto a la ruta de tu archivo    
+            
+            //Voy a leer mi fichero
+            var lectorFicheros = new LectorFicheros();
+            //lectorFicheros.Leer(_filePath);
+            //lectorFicheros.Tamaño(_filePath);
+
+            //TODO
+            //Recoger el MIME de un archivo
+            lectorFicheros.LeerMIME(_filePath);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    
+}
