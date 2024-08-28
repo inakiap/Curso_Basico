a2b39e70b1584409c2bc5c39df917a7391c7275a
Ignacio Alfaro Pinedo <Ignacio.AlfaroPinedo@enghouse.com>
2024-08-28 18:29:59 +0200

    Explicación de Objeto, clase y delegación.

---
+++ Curso_Basico/Helpers/GestionDeEntradas.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_Basico.Helpers
+{
+    class GestionDeEntradas
+    {
+
+    }
+
+    public class Entrada
+    {
+        private int ID;
+        private const string Version = "Version Origenial";
+        private string Codigo;
+        private string Concierto;
+        private decimal Precio;
+        private bool Vendida = false;
+        private int Descuento;
+        private int DescuentoExtra;
+        
+        private Entrada()
+        {
+            
+        }
+
+        //Crear entradas por el promotor
+        public Entrada(decimal precio, string concierto, int descuento = 0)
+        {
+            ID = 1;
+            Codigo = "A111";
+            Concierto = concierto;
+            Precio = precio;
+            Vendida = false;
+            Descuento = descuento;
+        }
+
+        public Entrada(string concierto) : this( 100, concierto)
+        {
+            //El concierto de vetusta lleva un 10% de descuento
+            if (Concierto.Equals("Vetusta"))
+            {
+                Descuento = 10;
+                DescuentoExtra = 5;
+            }
+
+            //El concierto de Aitana lleva un 5% de descuento extra
+            if (Concierto.Equals("Aitana"))
+            {
+                Descuento = 0;
+                DescuentoExtra = 5;
+            }
+        }
+
+        public bool EstaVendida()
+        {
+            return Vendida;
+        }
+
+        public int CualEsElDescuento()
+        {
+            return Descuento;
+        }
+
+        public int CualEsElDescuentoExtra()
+        {
+            return DescuentoExtra;
+        }
+
+        public decimal CualEsElPrecio()
+        {
+            return Precio;
+        }
+
+        public decimal CualEsElPrecioFinal()
+        {
+            return CalcularDescuento(this.CualEsElDescuentoExtra());
+        }
+
+        private decimal CalcularDescuento(int extra = 0)
+        {
+            // Init variables
+            decimal precio = this.CualEsElPrecio();
+            int descuento = this.CualEsElDescuento() + extra; //Los descuentos no se suelen aplicar así.
+
+            if (descuento > 0)
+            {
+                precio -= ((precio * descuento) / 100); //Se puede simplificar porque tiene 2 operaciones!!!!!!
+            }
+            return precio;
+        }
+
+        public static string ObtenerVersion()
+        {
+            return Version;
+        }
+
+    }
+}
--- Curso_Basico/Program.cs
+++ Curso_Basico/Program.cs
@@ -9,6 +9,25 @@ namespace Curso_Basico
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Entrada.ObtenerVersion());
+
+            Entrada mientrada = new Entrada("Aitana");
+
+            Console.WriteLine($"El precio es: {mientrada.CualEsElPrecio()}");
+            Console.WriteLine($"El descuento es de: {mientrada.CualEsElDescuento()}");
+            Console.WriteLine($"El descuento extra es de: {mientrada.CualEsElDescuentoExtra()}");
+            Console.WriteLine($"El precio con descuento es: {mientrada.CualEsElPrecioFinal()}");
+            
+
+            if (mientrada.EstaVendida())
+            {
+                Console.WriteLine("La entrada Sí está vendida.");
+            }
+            else
+            {
+                Console.WriteLine("La entrada No está vendida.");
+            }
+            
             //List<string> archivos = new List<string>();
             //archivos.Add("C:\\Test\\no_existe.zip");
             //archivos.Add("C:\\Test\\Wf_iAhorro.zip");
@@ -19,24 +38,24 @@ namespace Curso_Basico
             //archivos.Add("C:\\Test\\SRI_Deployment_Generic.xlsx"); // Error, dice que es un ZIP   
             
 
-            //Voy a leer mi fichero
-            var lectorFicheros = new LectorFicheros();
-            //lectorFicheros.Leer(_filePath);
-            //lectorFicheros.Tamaño(_filePath);
+            ////Voy a leer mi fichero
+            //var lectorFicheros = new LectorFicheros();
+            ////lectorFicheros.Leer(_filePath);
+            ////lectorFicheros.Tamaño(_filePath);
 
-            var a = lectorFicheros._FormatearTamaño(43432532,10);
-            var b = lectorFicheros._FormatearTamaño(ulong.MaxValue,10);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            //TODO
-            //Recoger el MIME de un archivo
-            //foreach (var item in archivos)
-            //{
-            //    Console.WriteLine(item);
-            //    //lectorFicheros.Leer(_filePath);
-            //    lectorFicheros.Tamaño(item);
-            //    //lectorFicheros.LeerMIME(item);
-            //}
+            //var a = lectorFicheros._FormatearTamaño(43432532,10);
+            //var b = lectorFicheros._FormatearTamaño(ulong.MaxValue,10);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            ////TODO
+            ////Recoger el MIME de un archivo
+            ////foreach (var item in archivos)
+            ////{
+            ////    Console.WriteLine(item);
+            ////    //lectorFicheros.Leer(_filePath);
+            ////    lectorFicheros.Tamaño(item);
+            ////    //lectorFicheros.LeerMIME(item);
+            ////}
 
             Console.ReadKey();
         }
